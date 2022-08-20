b998fcfa00fa556a70f70d6dadaf11e6611078d8
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2022-08-20 20:18:15 +0200

    chore(git-crypt): add script for unlocking with Base64 key

---
+++ unlock.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+from base64 import b64decode
+
+parser = argparse.ArgumentParser(description='Unencrypts repository')
+
+parser.add_argument(
+    'key', type=str, help='Base64-encoded key to unlock repository')
+parser.add_argument('--path', type=argparse.FileType('wb'),
+                    default='../git-crypt-key', help="Path to the git-crypt key file (default: '../git-crypt-key')")
+
+args = parser.parse_args()
+
+key = b64decode(args.key)
+args.path.write(key)
+args.path.close()
+
+os.system(f"git-crypt unlock {args.path.name}")
